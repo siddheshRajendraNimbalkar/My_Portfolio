b459e72ed07813a7c6a9368732f499a99949b78f
Nimbalkar Siddhesh <nimbalkarsiddhesh29@gmail.com>
2025-06-22 21:10:59 +0530

    add

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -136,7 +136,7 @@ const Hero = () => {
     {
       icon: <StickyNote className="" size={20} />,
       text: 'Resume',
-      link: '/Nimbalkar.pdf',
+      link: '/Nimbalkar_Resume.pdf',
     }
   ]
 
